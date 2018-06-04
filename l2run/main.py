eeba90cf6fb17fa44b666c06eed7b1a3be9cb3bf
Emanuele Ghelfi <manughelfi1994@gmail.com>
2018-06-04 09:02:38 +0200

    Checkpoint and logdir created if not present in run.sh

--- l2run/main.py
+++ l2run/main.py
@@ -41,13 +41,21 @@ def pack_run_params(seed, noise_type, layer_norm, evaluation, flip_state,
 
 def run(seed, noise_type, layer_norm, evaluation, flip_state,
         full, action_repeat, fail_reward, exclude_centering_frame,
-        checkpoint_dir, log_dir, session_path, last_training_step, **kwargs):
+        checkpoint_dir, log_dir, session_path, last_training_step,
+        integrator_accuracy, experiment_name, **kwargs):
 
     # we don't directly specify timesteps for this script, so make sure that if we do specify them
     # they agree with the other parameters
     if kwargs['num_timesteps'] is not None:
         assert(kwargs['num_timesteps'] == kwargs['nb_epochs'] *
                kwargs['nb_epoch_cycles'] * kwargs['nb_rollout_steps'])
+
+    tmp_log, tmp_chkpt = get_log_and_checkpoint_dirs(experiment_name)
+
+    if log_dir is None:
+        log_dir = tmp_log
+    if checkpoint_dir is None:
+        checkpoint_dir = tmp_chkpt
     param_noise = None
     # Configure things.
     rank = MPI.COMM_WORLD.Get_rank()
@@ -56,7 +64,7 @@ def run(seed, noise_type, layer_norm, evaluation, flip_state,
 
     # Main env
     env = create_environment(False, full, action_repeat,
-                             fail_reward, exclude_centering_frame)
+                             fail_reward, exclude_centering_frame, integrator_accuracy)
     env.reset()
     eval_env = None
 
@@ -100,7 +108,8 @@ def run(seed, noise_type, layer_norm, evaluation, flip_state,
                    actor=actor, critic=critic, memory=memory,
                    visualize=False, full=full, action_repeat=action_repeat,
                    fail_reward=fail_reward, exclude_centering_frame=exclude_centering_frame,
-                   learning_session=learning_session, ** kwargs)
+                   learning_session=learning_session, integrator_accuracy=integrator_accuracy,
+                   **kwargs)
 
     env.close()
     if eval_env is not None:
@@ -109,14 +118,16 @@ def run(seed, noise_type, layer_norm, evaluation, flip_state,
         logger.info('total runtime: {}s'.format(time.time() - start_time))
 
 
-def test(seed, layer_norm, full, action_repeat, fail_reward, exclude_centering_frame, **kwargs):
+def test(seed, layer_norm, full, action_repeat, fail_reward, exclude_centering_frame,
+        integrator_accuracy, render, **kwargs):
     # Configure things.
     rank = MPI.COMM_WORLD.Get_rank()
     if rank != 0:
         logger.set_level(logger.DISABLED)
     # Main env
-    env = create_environment(True, full, action_repeat,
-                             fail_reward, exclude_centering_frame)
+    env = create_environment(render, full, action_repeat,
+                             fail_reward, exclude_centering_frame,
+                             integrator_accuracy)
     env.reset()
     eval_env = None
 
@@ -180,6 +191,7 @@ def build_train_args(sub_parsers):
     parser.add_argument('--seed', help='RNG seed', type=int, default=0)
     parser.add_argument('--critic-l2-reg', type=float, default=1e-2)
     parser.add_argument('--batch-size', type=int, default=64)  # per MPI worker
+    parser.add_argument('--min-buffer-length', type=int, default=200) 
     # parser.add_argument('--actor-lr', type=float, default=1e-4)
     # parser.add_argument('--critic-lr', type=float, default=1e-3)
     boolean_flag(parser, 'popart', default=False)
@@ -213,11 +225,16 @@ def build_train_args(sub_parsers):
     boolean_flag(parser, 'exclude-centering-frame', default=False,
                  help="exclude pelvis from observation vec")
     parser.add_argument('--fail-reward', type=float, default=-0.2)
+    # Reduce accuracy -> Improve efficiency
+    # Good value may be 3e-2
+    parser.add_argument('--integrator-accuracy', type=float, default=5e-5)
+    # restore environment after some trajectories (possible mem leak)
+    parser.add_argument('--max-env-traj', type=int, default=100)
     # Learning session args
-    parser.add_argument('--session_path', required=True, type=str)
-    parser.add_argument('--checkpoint_dir', required=True, type=str)
-    parser.add_argument('--log_dir', required=True, type=str)
-    parser.add_argument('--training_step', type=int, default=0)
+    parser.add_argument('--session_path', type=str, default="log")
+    parser.add_argument('--checkpoint_dir', type=str)
+    parser.add_argument('--log_dir', type=str)
+    parser.add_argument('--last-training_step', type=int, default=0)
 
 
 def build_test_args(sub_parsers):
@@ -225,7 +242,7 @@ def build_test_args(sub_parsers):
     parser.set_defaults(func=test)
     # boolean_flag(parser, 'render-eval', default=False)
     boolean_flag(parser, 'layer-norm', default=True)
-    # boolean_flag(parser, 'render', default=False)
+    boolean_flag(parser, 'render', default=True)
     boolean_flag(parser, 'normalize-observations', default=False)
     parser.add_argument('--seed', help='RNG seed', type=int, default=0)
     parser.add_argument('--gamma', type=float, default=0.99)
@@ -239,6 +256,7 @@ def build_test_args(sub_parsers):
     boolean_flag(parser, 'full', default=False, help="use full observation")
     boolean_flag(parser, 'exclude-centering-frame', default=False,
                  help="exclude pelvis from observation vec")
+    parser.add_argument('--integrator-accuracy', type=float, default=5e-5)
     parser.add_argument('--fail-reward', type=float, default=-0.2)
     parser.add_argument('--checkpoint-dir', type=str)
 
@@ -251,6 +269,18 @@ def build_resume_args(sub_parsers):
     parser.add_argument('--dump-file', type=str,
                         help='The learning session to resume')
 
+def get_log_and_checkpoint_dirs(experiment_name):
+    import datetime
+
+    now = datetime.datetime.now()
+    log_dir = "tf_logs/" + experiment_name + \
+        "-" + now.strftime("%Y%m%d-%H%M%S") + "/"
+    checkpoint_dir = "tf_checkpoints/" + experiment_name + \
+        "-" + now.strftime("%Y%m%d-%H%M%S")
+    if not os.path.isdir(checkpoint_dir):
+        os.makedirs(checkpoint_dir)
+    checkpointfile = checkpoint_dir + "/model"
+    return log_dir, checkpointfile
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
