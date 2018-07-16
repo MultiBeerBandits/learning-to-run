edc3d39ca87b6e40586b22c9e4cde74d5af130ea
Emanuele Ghelfi <manughelfi1994@gmail.com>
2018-07-16 09:27:17 +0200

    Changed to fixed workers

--- l2run/pois/runcbpois.py
+++ l2run/pois/runcbpois.py
@@ -38,7 +38,7 @@ def train(num_episodes, horizon, iw_method, iw_norm, natural, bound, delta, seed
     def make_policy(name, ob_space, ac_space):
         return MlpPolicy(name=name, ob_space=ob_space, ac_space=ac_space,
                          hid_size=hid_size, num_hid_layers=num_hid_layers, gaussian_fixed_var=True, use_bias=False, use_critic=False,
-                         clip_ob=False, use_rms=False)
+                          use_rms=False)
 
     sampler = ParallelSampler(make_policy, make_env, num_episodes, horizon, True, n_workers=njobs, seed=seed)
 
